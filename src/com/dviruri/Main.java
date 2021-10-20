068de65473b788afffa9fde15b4e1ce5dbff8e89
Uri Dvir <Uri.Dvir@microfocus.com>
2021-10-20 09:48:36 +0300

    Second Commit

--- src/com/dviruri/Main.java
+++ src/com/dviruri/Main.java
@@ -3,6 +3,6 @@ package com.dviruri;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is my first program in JAVA");
+        System.out.println("This is my first program in JAVA - 2");
     }
 }
